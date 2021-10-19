9a1027680ff98fb598430845d0ea1b7c3a5a7d81
mmcaulif <mmcaulif@tcd.ie>
2021-10-19 12:42:30 +0100

    began working on experience replay, hopefully just using jax

--- RL_dqn.py
+++ RL_dqn.py
@@ -1,4 +1,7 @@
 import os
+
+import numpy as np
+
 os.environ.setdefault('JAX_PLATFORM_NAME', 'cpu')  # tell JAX to use CPU, cpu is faster on small networks
 
 import jax
@@ -9,9 +12,32 @@ from collections import namedtuple
 import optax
 import coax
 import gym
+import sys
 
-tracer = coax.reward_tracing.NStep(n=1, gamma=0.9)
-replay = coax.experience_replay.SimpleReplayBuffer(capacity=100000)
+experience = jnp.zeros((0, 4))
+
+print(jnp.shape(experience))
+a = b = c = 1
+d = jnp.array([[1,1]])
+ones = jnp.array([[a,b,c,d]])
+print(jnp.shape(ones))
+experience = jnp.append(experience, ones, axis = 0)
+print(experience, len(experience))
+
+sys.exit()
+
+
+def ReplayBuffer(exp, s, a, r, s_next):
+    if len(exp) <= 100000:
+        transition = jnp.array([[s, a, r, s_next]], dtype=int)
+        exp = jnp.append(exp, transition, axis=0)
+        return exp
+
+    else:
+        exp = jnp.delete(exp, 0)
+        transition = jnp.array((s, a, r, s_next))
+        exp = jnp.append(exp, transition, axis=0)
+        return exp
 
 env = gym.make('CartPole-v1')
 
@@ -25,29 +51,26 @@ def func(S):
     ])
     return seq(S)
 
-obs = env.reset()
-
-s_t = obs
-
 params = func.init(jax.random.PRNGKey(42), jnp.ones(4))
 
 forward = jax.jit(hk.without_apply_rng(func).apply)
 
-q_table = forward(params, obs)
+#q_table = forward(params, s_t)
+#a_t = int(jnp.argmax(q_table))
+#s_tp1, r_t, done, _ = env.step(a_t)
 
-a_t = int(jnp.argmax(q_table))
+s_t = env.reset()
+for i in range(100):
+    a_t = env.action_space.sample() # your agent here (this takes random actions)
+    s_tp1, r_t, done, _ = env.step(a_t)
+    print(i)
+    experience = ReplayBuffer(experience, s_t, a_t, r_t, s_tp1)
 
-s_tp1, r_t, d, _ = env.step(a_t)
+    s_t = s_tp1
 
-tracer.add(s_t, a_t, r_t, d, s_tp1)
+    if done:
+        s_t = env.reset()
 
-while tracer:
-    replay.add(tracer.pop())
+env.close()
 
-print(replay)
-
-#print(replay.sample(batch_size=1))
-
-for i in range(10000):
-    while not d:
-        obs = env.reset()
+print(experience)
